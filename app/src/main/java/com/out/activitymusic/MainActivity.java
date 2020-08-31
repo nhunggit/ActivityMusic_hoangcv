58ccbb8c0631f846e47dee3cba059ff95a6ef6b8
CAO VĂN HOÀNG <caohoang472000@gmail.com>
2020-08-31 16:45:48 +0700

    test

--- app/src/main/java/com/out/activitymusic/MainActivity.java
+++ app/src/main/java/com/out/activitymusic/MainActivity.java
@@ -30,29 +30,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //final ListView list = findViewById(R.id.list_view);
 
-
-
-
-        FragmentManager fragmentManager=getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-        AllSongsFragment allSongsFragment=new AllSongsFragment();
+     /*   AllSongsFragment allSongsFragment=new AllSongsFragment();
         fragmentTransaction.replace(R.id.frame_Layout1,allSongsFragment);
         fragmentTransaction.commit();
         FragmentManager fragmentManager1=getSupportFragmentManager();
         FragmentTransaction fragmentTransaction1=fragmentManager1.beginTransaction();
         MediaPlaybackFragment mediaPlaybackFragment= new MediaPlaybackFragment();
         fragmentTransaction1.replace(R.id.frame_Layout2,mediaPlaybackFragment);
-        fragmentTransaction1.commit();
+        fragmentTransaction1.commit();*/
+/*
+        drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);*/
+/*
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
 
+        AllSongsFragment allSongsFragment = new AllSongsFragment();
+        if (findViewById(R.id.frame_Layout) != null) {
+            // Found the ID of only one Fragment ==> Portrait mode
+            // Remove the existing fragment before add new one
+            if (savedInstanceState != null) {
+                getSupportFragmentManager().executePendingTransactions();
+                Fragment fragmentById = getSupportFragmentManager().findFragmentById(R.id.frame_Layout);
+                if (fragmentById != null) {
+                    getSupportFragmentManager().beginTransaction().remove(fragmentById).commit();
+                }
+            }
 
+            // Add new one
+            getSupportFragmentManager().beginTransaction().add(R.id.frame_Layout, allSongsFragment).commit();
+        } else {
+            // Landscape mode
+            // Remove the existing fragments before add new one
+            if (savedInstanceState != null) {
+                getSupportFragmentManager().executePendingTransactions();
+                Fragment firstFragmentById = getSupportFragmentManager().findFragmentById(R.id.frame_Layout1);
+                if (firstFragmentById != null) {
+                    getSupportFragmentManager().beginTransaction().remove(firstFragmentById).commit();
+                }
+                Fragment secondFragmentById = getSupportFragmentManager().findFragmentById(R.id.frame_Layout2);
+                if (secondFragmentById != null) {
+                    getSupportFragmentManager().beginTransaction().remove(secondFragmentById).commit();
+                }
+            }
+
+            // Add new one
+            getSupportFragmentManager().beginTransaction().add(R.id.frame_Layout1, allSongsFragment).commit();
+        }
 
 
 
     }
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_seach, menu);
-        return true;
-    }
-
+    public void onItemPressed(String content) {
+        @Override
+        MediaPlaybackFragment mediaPlaybackFragment = MediaPlaybackFragment.newInstance(content);
+        public boolean onCreateOptionsMenu(Menu menu)
+        {
+            if (findViewById(R.id.frame_Layout2) != null) {
+                getMenuInflater().inflate(R.menu.menu_seach, menu);
+                // Found the ID of only one Fragment ==> Portrait mode    return true;
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame_Layout, mediaPlaybackFragment);
+            }
+        }  }*/
 }
--- app/src/main/java/com/out/activitymusic/MediaPlaybackFragment.java
+++ app/src/main/java/com/out/activitymusic/MediaPlaybackFragment.java
@@ -11,6 +11,11 @@ import androidx.fragment.app.Fragment;
 
 public class MediaPlaybackFragment extends Fragment {
     View mInflater;
+
+    public static MediaPlaybackFragment newInstance(String content) {
+        return MediaPlaybackFragment.newInstance(content);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
